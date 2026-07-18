b39f831e79425cdb8e3287dfdc5d24414e2fae2c
Reine18 <wendkuni.reine.rosine.guinguiere@umontreal.ca>
2026-07-18 18:18:42 -0400

    corrections routes backend

--- src/routes/ambianceRoutes.js
+++ src/routes/ambianceRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
+
 const {
   getAmbianceSummary,
   getAmbianceHistory,
   getQuietHours,
+  getAmbianceForecast,
 } = require("../controllers/ambianceController");
 
 const router = express.Router();
 
 router.get("/:location/summary", getAmbianceSummary);
+
 router.get("/:location/history", getAmbianceHistory);
+
 router.get("/:location/quiet-hours", getQuietHours);
+
+router.get("/:location/forecast", getAmbianceForecast);
 
 module.exports = router;
