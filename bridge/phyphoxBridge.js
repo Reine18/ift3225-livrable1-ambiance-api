8780d27f30a636ca66cf6af6ceaca95a7fc92444
mmm777777 <145052733+mmm777777@users.noreply.github.com>
2026-07-21 17:41:11 -0400

    modifi bridge

--- bridge/phyphoxBridge.js
+++ bridge/phyphoxBridge.js
@@ -1,27 +1,15 @@
 require("dotenv").config();
 const axios = require("axios");
 
-
-// API backend
 const API_URL = "http://localhost:3000/measurements";
-
-// Clé API générée par POST /devices
-const API_KEY = process.env.DEVICE_API_KEY || "On mettra chacune nos API_KEY dans un .env séparé pour pas que ça traîne sur GitHub";
-
-// Adresse affichée par Phyphox sur telephone de celle qui envoie les données( Reine/Meriem/Samah)
+const API_KEY = process.env.DEVICE_API_KEY;
+const DEVICE_ID = process.env.DEVICE_ID;
 const PHYPHOX_BASE_URL = process.env.PHYPHOX_BASE_URL;
-
-// Endpoint Phyphox pour l'intensité sonore
 const PHYPHOX_URL = `${PHYPHOX_BASE_URL}/get?dbUncal`;
 
 const sendMeasurement = async () => {
   try {
     const response = await axios.get(PHYPHOX_URL);
-
-    console.log(
-      "Données Phyphox reçues :",
-      JSON.stringify(response.data, null, 2)
-    );
 
     if (!response.data.status?.measuring) {
       console.log("Phyphox arrêté : mesure non envoyée.");
@@ -35,22 +23,14 @@ const sendMeasurement = async () => {
       return;
     }
 
-    const rawValue =
-  dbData[dbData.length - 1];
+    const rawValue = dbData[dbData.length - 1];
 
-if (
-  rawValue === null ||
-  rawValue === undefined
-) {
-  console.log(
-    "Mesure ignorée : valeur nulle"
-  );
-  return;
-}
+    if (rawValue === null || rawValue === undefined) {
+      console.log("Mesure ignorée : valeur nulle");
+      return;
+    }
 
-const soundLevel = Number(rawValue);
-
-
+    const soundLevel = Number(rawValue);
 
     if (Number.isNaN(soundLevel)) {
       console.log("Mesure ignorée : valeur invalide.");
@@ -58,14 +38,11 @@ const soundLevel = Number(rawValue);
     }
 
     const measurement = {
+      deviceId: DEVICE_ID,
       soundLevel,
       amplitude: Math.abs(soundLevel),
       timestamp: new Date().toISOString(),
     };
-
-
-
-
 
     const apiResponse = await axios.post(API_URL, measurement, {
       headers: {
