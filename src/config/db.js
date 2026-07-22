e50a464c1fdf70b48db39a14f122b32fab37aecc
Reine18 <wendkuni.reine.rosine.guinguiere@umontreal.ca>
2026-07-21 20:54:44 -0400

    Afficher la base MongoDB utilisée

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connecté");
+    console.log("Base utilisée :", connection.connection.name);
   } catch (error) {
     console.error("Erreur MongoDB :", error.message);
     process.exit(1);
